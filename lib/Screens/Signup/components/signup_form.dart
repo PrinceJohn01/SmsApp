de827503276bf9f390fc58f6b14c2b94316a2e4b
PrinceJohn01 <princedav748@gmail.com>
2023-06-24 23:23:18 +0100

    b

--- lib/Screens/Signup/components/signup_form.dart
+++ lib/Screens/Signup/components/signup_form.dart
@@ -4,24 +4,35 @@ import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Login/login_screen.dart';
 
-class SignUpForm extends StatelessWidget {
-   SignUpForm({
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({
     Key? key,
   }) : super(key: key);
 
-   bool _obscurePassword = true;
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
 
-  //void _togglePasswordVisibility() {
-  //  setState(() {
-      //_obscurePassword = !_obscurePassword;
-   // });
-  //}
+class _SignUpFormState extends State<SignUpForm> {
+  bool _obscurePassword = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscurePassword = !_obscurePassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
+          const Align(
+            alignment: Alignment.centerLeft,
+            child:
+            Text("Email",style: TextStyle(
+                color: Colors.black,
+                fontSize: 16))),
           TextFormField(
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
@@ -34,47 +45,55 @@ class SignUpForm extends StatelessWidget {
               ),
             ),
           ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
+      const SizedBox(height: defaultPadding),
+      const Align(
+        alignment: Alignment.centerLeft,
+        child:
+        Text('Password', style: TextStyle(
+            color: Colors.black,
+            fontSize: 16))),
+              TextFormField(
               textInputAction: TextInputAction.done,
-              obscureText: true,
+              obscureText: _obscurePassword,
               cursorColor: kPrimaryColor,
-              decoration:  InputDecoration(
+              decoration: InputDecoration(
+                labelText: "Password",
                 hintText: "Password",
                 suffixIcon: IconButton(
-                  icon: Icon(_obscurePassword ? Icons.visibility : Icons.visibility_off),
-                  onPressed:() {},
+                  icon: Icon(
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: _togglePasswordVisibility,
+                ),
+                prefixIcon: const Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                ),
+              ),
+          ),
+      const SizedBox(height: defaultPadding),
+      const Align(
+        alignment: Alignment.centerLeft,
+        child:
+        Text('Confirm password', style: TextStyle(
+            color: Colors.black,
+            fontSize: 16))),
+            TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: _obscurePassword,
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                hintText: "Password",
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: _togglePasswordVisibility,
                 ),
                 prefixIcon: const Padding(
                   padding: EdgeInsets.all(defaultPadding),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
-            child:
-            TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
-              cursorColor: kPrimaryColor,
-              decoration:  InputDecoration(
-                hintText: "Password",
-                suffixIcon: IconButton(
-                  icon: Icon(_obscurePassword ? Icons.visibility : Icons.visibility_off),
-                  onPressed: () {} ,
-                ),
-
-                prefixIcon: Padding(
-                  padding: EdgeInsets.all(defaultPadding),
-                ),
-              ),
-            ),
-          ),
-
-
           const SizedBox(height: defaultPadding / 3),
           ElevatedButton(
             onPressed: () {},
