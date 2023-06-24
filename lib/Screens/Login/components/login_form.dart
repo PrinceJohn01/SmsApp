19620e679263db5726880907ea4361dba0853f06
PrinceJohn01 <princedav748@gmail.com>
2023-06-24 23:23:18 +0100

    b

--- lib/Screens/Login/components/login_form.dart
+++ lib/Screens/Login/components/login_form.dart
@@ -4,18 +4,35 @@ import '../../../components/already_have_an_account_acheck.dart';
 import '../../../constants.dart';
 import '../../Signup/signup_screen.dart';
 
-class LoginForm extends StatelessWidget {
-   LoginForm({
+class LoginForm extends StatefulWidget {
+  const LoginForm({
     Key? key,
   }) : super(key: key);
 
+   _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
   bool _obscurePassword = true;
+  bool _rememberMe = false;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscurePassword = !_obscurePassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
       child: Column(
         children: [
+        const Align(
+        alignment: Alignment.centerLeft,
+        child:
+           Text("Email",style: TextStyle(
+        color: Colors.black,
+        fontSize: 16))),
           TextFormField(
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
@@ -27,23 +44,30 @@ class LoginForm extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding:  const EdgeInsets.symmetric(vertical: defaultPadding),
-            child: TextFormField(
+          const SizedBox(height: defaultPadding),
+       const Align(
+        alignment: Alignment.centerLeft,
+        child:
+             Text('Password', style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,fontStyle: FontStyle.italic))),
+            TextFormField(
               textInputAction: TextInputAction.done,
-              obscureText: true,
+              obscureText: _obscurePassword,
+              cursorColor: kPrimaryColor,
               decoration:  InputDecoration(
                 hintText: "Password",
                 suffixIcon: IconButton(
-                    icon: Icon(_obscurePassword ? Icons.visibility : Icons.visibility_off),
-                    onPressed:() {},
+                    icon: Icon(_obscurePassword ? Icons.visibility : Icons.visibility_off
+                    ),
+                    onPressed:_togglePasswordVisibility,
                 ),
                 prefixIcon: const Padding(
                   padding: EdgeInsets.all(defaultPadding),
                 ),
               ),
             ),
-          ),
+
           const SizedBox(height: defaultPadding),
           Hero(
             tag: "login_btn",
@@ -54,6 +78,34 @@ class LoginForm extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(height: defaultPadding),
+
+          Row(
+            children: [
+              Checkbox(
+                value: _rememberMe,
+                onChanged: (value) {
+                  setState(() {
+                    _rememberMe = value!;
+                  });
+                },
+              ),
+               const Text("Remember Me"),
+              const Spacer(),
+              GestureDetector(
+                onTap: () {
+                  // Handle "Forget Password?" text onPressed callback.
+                },
+                child: const Text(
+                  "Forget Password?",
+                  style: TextStyle(
+                    color: Colors.black38,
+                  ),
+                ),
+              ),
+            ],
+          ),
+
           const SizedBox(height: defaultPadding),
           AlreadyHaveAnAccountCheck(
             press: () {
--- lib/Screens/Login/login_screen.dart
+++ lib/Screens/Login/login_screen.dart
@@ -9,13 +9,13 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Background(
+    return  const Background(
       child: SingleChildScrollView(
         child: Responsive(
-          mobile: const MobileLoginScreen(),
+          mobile: MobileLoginScreen(),
           desktop: Row(
             children: [
-              const Expanded(
+              Expanded(
                 child: LoginScreenTopImage(),
               ),
               Expanded(
@@ -44,18 +44,18 @@ class MobileLoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return  const Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        const LoginScreenTopImage(),
+        LoginScreenTopImage(),
         Row(
           children: [
-            const Spacer(),
+            Spacer(),
             Expanded(
               flex: 8,
               child: LoginForm(),
             ),
-            const Spacer(),
+            Spacer(),
           ],
         ),
       ],
